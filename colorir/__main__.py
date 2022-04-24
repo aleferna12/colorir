0b4c1feabf8df54f3dd9169246cd56db1954e07c
aleferna12 <aleferna2001@hotmail.com>
2022-04-24 17:17:59 +0100

    Restricted choices for exemples

--- colorir/__main__.py
+++ colorir/__main__.py
@@ -4,9 +4,10 @@ from importlib import import_module
 from pathlib import Path
 from colorir import config
 
+example_path = Path(__file__).resolve().parent.parent / "examples"
 
-def run_example_app(example):
-    example_path = Path(__file__).resolve().parent.parent / "examples"
+
+def main(example):
     sys.path.append(str(example_path))
     config.DEFAULT_PALETTES_DIR = str(example_path / "ex_palettes")
     import_module(example)
@@ -15,8 +16,9 @@ def run_example_app(example):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="python -m colorir",
                                      description="Execute example applications.")
+    examples_choices = [file.name.replace(".py", "") for file in example_path.glob("*py")]
     parser.add_argument("app",
-                        help="which example application to execute (e.g.: simple_turtle_2)")
-
+                        help="which example application to execute (e.g.: simple_turtle_2)",
+                        choices=examples_choices)
     args = parser.parse_args()
-    run_example_app(args.app)
+    main(args.app)
