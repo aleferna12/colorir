26b05aab444c76a6fa5fab44fc4ee0c017ead355
aleferna12 <aleferna2001@hotmail.com>
2022-06-14 18:46:25 +0100

    Added tests for distance functions

--- tests/test_color.py
+++ tests/test_color.py
@@ -1,11 +1,17 @@
 import doctest
 import unittest
-from colorir import color
+from colorir import color, sRGB, simplified_dist
 
 
 def load_tests(loader, tests, ignore):
     tests.addTests(doctest.DocTestSuite(color))
     return tests
+
+
+class TestDistance(unittest.TestCase):
+    def test_simple_dist(self):
+        dist = simplified_dist(sRGB(255, 255, 255), sRGB(0, 0, 0))
+        self.assertAlmostEqual(dist, 765)
 
 
 if __name__ == "__main__":
