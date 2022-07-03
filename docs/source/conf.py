bfa258c7bb1f6fb77436c50fd0c9381afdca03b0
aleferna12 <aleferna2001@hotmail.com>
2022-07-03 18:14:05 +0100

    Updated version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyrimaght = '2022, Alexandre Fernandes'
 author = 'Alexandre Fernandes'
 
 # The full version, including alpha/beta/rc tags
-release = '1.2.4'
+release = '1.2.5'
 
 
 # -- General configuration ---------------------------------------------------
