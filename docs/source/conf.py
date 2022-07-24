9bcc7508c6ffd64520a1ebf1a2220150f094985d
aleferna12 <aleferna2001@hotmail.com>
2022-07-24 02:47:37 -0300

    Added necessary imports

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,6 +30,7 @@ release = '1.3.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+import sphinx_rtd_theme
 extensions = [
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
