6e1309275db19c155626daa82eb27355d35130bc
aleferna12 <aleferna2001@hotmail.com>
2022-08-14 23:17:49 -0300

    Updated vers

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Alexandre Fernandes'
 author = 'Alexandre Fernandes'
 
 # The full version, including alpha/beta/rc tags
-release = '1.3.11'
+release = '1.3.12'
 
 
 # -- General configuration ---------------------------------------------------
