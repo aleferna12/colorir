a40c7d66cfac4b22b374f31bae8edad476431628
aleferna12 <aleferna2001@hotmail.com>
2025-02-13 18:07:38 +0000

    Version bump

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2024, Alexandre Fernandes'
 author = 'Alexandre Fernandes'
 
 # The full version, including alpha/beta/rc tags
-release = '2.1.2'
+release = '2.1.3'
 
 
 # -- General configuration ---------------------------------------------------
